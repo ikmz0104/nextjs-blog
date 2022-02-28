4a2b72ae93895de313e0a022bf05e7981406cc5f
ikmz <ikmz0104.official@gmail.com>
2022-02-28 23:35:31 +0900

    [fix] 軽微な修正

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,16 +1,16 @@
-function Error({ statusCode }) {
-    return (
-      <p>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    )
-  }
-  
-  Error.getInitialProps = ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    return { statusCode }
-  }
-  
-  export default Error
+import React from 'react';
+import { NextPage, NextPageContext } from 'next';
+interface Props {
+    statusCode: number;
+}
+const Error: NextPage<Props> = ({ statusCode }) => {
+  return <div>{statusCode}エラーが発生しました</div>;
+};
+
+Error.getInitialProps = async ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode ?? 500 : 404;
+
+  return { statusCode };
+};
+
+export default Error;
